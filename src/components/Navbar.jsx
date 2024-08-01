d017cf2226cfa0d3527c73710171e6124fba0ed3
zobeir-rigi <rigi.zobir@yahoo.com>
2024-08-01 13:54:06 +0100

    navbar complited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
@@ -8,6 +9,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px;
     display:flex;
+    align-items:center;
     justify-content:space-between;
 `;
 
@@ -24,15 +26,36 @@ const Language = styled.span`
 `;
 
 const SearchContainer = styled.div`
-    border: 1px solid lightgray;
-`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items:center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+const Input = styled.input`
+    border: none;
+`;
 
 const Center = styled.div`
-    flex:1
-`
+    flex:1;
+    text-align: center;
+`;
+const Logo = styled.h1`
+    font-weight: bold;
+
+`;
 const Right = styled.div`
-     flex:1
-`
+     flex:1;
+     display: flex;
+     align-items:center ;
+     justify-content: flex-end;
+`;
+
+const MenuItems = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+`;
 
 
 
@@ -43,12 +66,22 @@ const Navbar = ()=> {
                 <Left>
                 <Language>EN</Language>
                 <SearchContainer>
-                input
-                <Search/>
+                    <Input/>
+                    <Search style={{color:"gray", fontSize:"16px"}} />
                 </SearchContainer>
                 </Left>
-                <Center>center</Center>
-                <Right>right</Right>
+                <Center>
+                    <Logo>ICDI.</Logo>
+                </Center>
+                <Right>
+                    <MenuItems>REGISTER</MenuItems>
+                    <MenuItems>SIGN IN</MenuItems>
+                    <MenuItems>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItems>
+                </Right>
             </Wrapper>
         </Container>
     )
