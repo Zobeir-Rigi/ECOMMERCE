8ad9d04a49200265d35555ada82590deb1722c06
zobeir-rigi <rigi.zobir@yahoo.com>
2024-08-05 19:43:56 +0100

    one slide created

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -24,15 +24,64 @@ const Arrow = styled.div`
     cursor: pointer;
     opacity: 0.5;
 `;
+const Wrapper = styled.div`
+    height: 100%;
+`;
+const Slide = styled.div`
+    width: 100vm;
+    height: 100vh;
+    display: flex;
+    align-items:center;
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Image = styled.img`
+    height: 80%;
+`;
+
+const InfoContainer = styled.div`
+    flex:1;
+    padding: 50px;
+`;
+const Title = styled.h1`
+    font-size: 70px;
+`;
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`;
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color:transparent;
+    cursor: pointer;
+`;
+
 const Slider = () =>{
     return(
         <Container>
-        <Arrow direction="left">
-            <ArrowLeftOutlined />
-        </Arrow>
-        <Arrow direction="right">
-            <ArrowRightOutlined />
-        </Arrow>
+            <Arrow direction="left">
+                <ArrowLeftOutlined />
+            </Arrow>
+            <Wrapper>
+                <Slide>
+                    <ImgContainer>
+                        <Image src="https://image.hm.com/assets/hm/4f/42/4f428bfd339df4a2255e9639845aa1a52f9aa447.jpg?imwidth=1536"/>
+                    </ImgContainer>
+                    <InfoContainer>
+                        <Title>Summer sall</Title>
+                        <Desc>good Quality and price, Hurry up to take one !!!</Desc>
+                        <Button>SHOP NOW</Button>
+                    </InfoContainer>
+                </Slide>
+            </Wrapper>
+            <Arrow direction="right">
+                <ArrowRightOutlined />
+            </Arrow>
         </Container>
     )
 }
