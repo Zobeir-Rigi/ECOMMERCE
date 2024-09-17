52285dbf913eef6342785f82ae415c5055f736e8
zobeir-rigi <rigi.zobir@yahoo.com>
2024-09-18 00:35:07 +0100

    summary section done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,39 +1,36 @@
 import styled from "styled-components";
-import Navbar from "../components/Navbar"
-import Announcement from "../components/Announcement"
-import Footer from "../components/Footer"
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
 import { Add, Remove } from "@material-ui/icons";
 
-const Container = styled.div`
-    
-`
+const Container = styled.div``;
 
 const Warpper = styled.div`
-    padding: 20px;
-`
+  padding: 20px;
+`;
 const Title = styled.h1`
-    font-weight: 300;
-    text-align: center;
-`
+  font-weight: 300;
+  text-align: center;
+`;
 const Top = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 20px;
-`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
 
 const TopButton = styled.button`
-    padding: 10px;
-    font-weight: 600;
-    cursor: pointer;
-    border: ${(props) => props.type === "filled" && "none"};
-    background-color: ${(props) =>
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
     props.type === "filled" ? "black" : "transparent"};
-    color: ${(props) => props.type === "filled" && "white"};
+  color: ${(props) => props.type === "filled" && "white"};
 `;
 
-const TopTexts = styled.div`
-`;
+const TopTexts = styled.div``;
 
 const TopText = styled.span`
   text-decoration: underline;
@@ -42,14 +39,13 @@ const TopText = styled.span`
 `;
 
 const Bottom = styled.div`
-    display: flex;
-    justify-content: space-between;
-`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Info = styled.div`
-    flex: 3;
-`
-
+  flex: 3;
+`;
 
 const Product = styled.div`
   display: flex;
@@ -115,28 +111,55 @@ const Hr = styled.hr`
   height: 1px;
 `;
 
-
 const Summary = styled.div`
-        flex: 1;
-`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
 
-const Cart = ()=>{
-    return (
-        <Container>
-            <Navbar />
-            <Announcement />
-            <Warpper>
-                <Title>YOUR BAG</Title>
-                <Top>
-                <TopButton>CONTINUE SHOPPING</TopButton>
-                <TopTexts>
-                  <TopText>Shopping Bag(2)</TopText>
-                  <TopText>Your Wishlist (0)</TopText>
-                </TopTexts>
-                <TopButton type="filled">CHECKOUT NOW</TopButton>
-                </Top>
-                <Bottom>
-                <Info>
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "500"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+
+const SummaryItemText = styled.span``;
+
+const SummaryItemPrice = styled.span``;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Warpper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
             <Product>
               <ProductDetail>
                 <Image src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1614188818-TD1MTHU_SHOE_ANGLE_GLOBAL_MENS_TREE_DASHERS_THUNDER_b01b1013-cd8d-48e7-bed9-52db26515dc4.png?crop=1xw:1.00xh;center,top&resize=480%3A%2A" />
@@ -155,7 +178,7 @@ const Cart = ()=>{
               </ProductDetail>
               <PriceDetail>
                 <ProductAmountContainer>
-                  <Add/>
+                  <Add />
                   <ProductAmount>2</ProductAmount>
                   <Remove />
                 </ProductAmountContainer>
@@ -189,13 +212,31 @@ const Cart = ()=>{
               </PriceDetail>
             </Product>
           </Info>
-                    <Summary>Summary</Summary>
-
-                </Bottom>
-            </Warpper>
-            <Footer />
-        </Container>
-    )
-} 
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Estimated Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText>Shipping Discount</SummaryItemText>
+              <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem type="total">
+              <SummaryItemText>Total</SummaryItemText>
+              <SummaryItemPrice>$ 80</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT NOW</Button>
+          </Summary>
+        </Bottom>
+      </Warpper>
+      <Footer />
+    </Container>
+  );
+};
 
 export default Cart;
