ff413da5e802d32b9fe25d8a92b26fb2d4849e86
zobeir-rigi <rigi.zobir@yahoo.com>
2024-09-19 04:04:19 +0100

    footer responsived

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 import {  } from '@material-ui/core';
 import { GitHub, Instagram, LinkedIn, MailOutline, Phone, Room, Twitter } from '@material-ui/icons';
+import { mobile } from "../responsive";
 
 const Container = styled.div`
     display: flex;
+    ${mobile({ flexDirection: "column" })}
 `
 const Left = styled.div`
     flex: 1;
@@ -34,6 +36,7 @@ const SocialIcon = styled.div`
 const Center = styled.div`
         flex: 1;
         padding: 20px;
+        ${mobile({ display: "none" })}
 `
 const Title = styled.h3`
     margin-bottom: 30px;
@@ -53,6 +56,7 @@ const ListItem = styled.li`
 const Right = styled.div`
         flex: 1;
         padding: 20px;
+        ${mobile({ backgroundColor: "#fff8f8" })}
 `
 const ContactItem = styled.div`
   margin-bottom: 20px;
