6f6cd31473a3e49067b06fa47708e1322502f89f
zobeir-rigi <rigi.zobir@yahoo.com>
2024-09-19 05:03:01 +0100

    react router dom created and routes are aligned

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import ProductList from "./pages/ProductList";
 import Cart from "./pages/Cart";
 import Register from "./pages/Register";
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 const App = ()=>{
-  return <Cart />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
